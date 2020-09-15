7234a8d8ff9c269d8c637bd8313c25d1fb944c9e
Maciej Niedzielski <mniedzielski.dev@gmail.com>
2020-09-15 21:16:24 +0200

    feat: add mongoose & connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 import routes from "./routes";
 import { logger, notFound } from "./middlewares";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const ENV = app.get("env");
+
 app.set("port", process.env.PORT || 3000);
 
 // Register routing
@@ -11,11 +15,16 @@ routes(app);
 // Register middlewares
 app.use([express.json(), logger, notFound]);
 
-const PORT = process.env.PORT || 3000;
-const ENV = app.get("env");
-
-app.listen(PORT, () => {
-  console.log(
-    `🚀 Server is running in http://localhost:${PORT} in ${ENV} mode`
-  );
-});
+mongoose
+  .connect("mongodb://localhost:27017/basketball-courts", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Server is running in http://localhost:${PORT} in ${ENV} mode`
+      );
+    });
+  })
+  .catch(() => console.log("Unable to connect to the database."));
